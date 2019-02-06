440b3e46ee5fdd2ceba17b622c4df576ac1f01d2
Sergey Bodnaryuk <s.bodnaryuk@gmail.com>
2019-02-06 10:16:13 +1000

    Исправляет js

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,21 @@
 'use strict';
 
 var slider = document.querySelector('.slider');
-var sliderControls = slider.querySelectorAll('.slider__control');
-var sliderSlides = slider.querySelectorAll('.slider__item');
 
-for (var i = 0; i < sliderControls.length; i++) {
-  sliderControls[i].addEventListener('click', function (event) {
-    for (var j = 0; j < sliderControls.length; j++) {
-      sliderControls[j].removeAttribute('disabled');
-      sliderSlides[j].classList.remove('slider__item--active');
-    }
-    event.currentTarget.setAttribute('disabled', 'disabled');
-    sliderSlides[Array.prototype.indexOf.call(
-        event.currentTarget.parentElement.children, event.currentTarget
-    )].classList.add('slider__item--active');
-  });
+if (slider) {
+  var sliderControls = slider.querySelectorAll('.slider__control');
+  var sliderSlides = slider.querySelectorAll('.slider__item');
+
+  for (var i = 0; i < sliderControls.length; i++) {
+    sliderControls[i].addEventListener('click', function (event) {
+      for (var j = 0; j < sliderControls.length; j++) {
+        sliderControls[j].removeAttribute('disabled');
+        sliderSlides[j].classList.remove('slider__item--active');
+      }
+      event.currentTarget.setAttribute('disabled', 'disabled');
+      sliderSlides[Array.prototype.indexOf.call(
+          event.currentTarget.parentElement.children, event.currentTarget
+      )].classList.add('slider__item--active');
+    });
+  }
 }
